3a3c6bff868011f7597bea86a64c8237d1dac202
jamil <jamil.saleh@student.moringaschool.com>
2023-02-08 08:25:36 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -1,23 +1,12 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+puts "Hello World!"
+#puts "Hello world!"
+#puts "Hello world!"
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+print "Pass this test, please."
+#print "Hello world!"
+pp [1,2,3,]
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+ 
+dogName = "lucy"
+puts "Say hello to my dog {dogName}"
